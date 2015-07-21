6c2f2e64af633312d1a5f7fcb12b64c1af34a07c
flareair <flareair0@gmail.com>
2015-07-21 13:07:26 +0300

    First test  now works

--- tests/ValuteDynamicTest.php
+++ tests/ValuteDynamicTest.php
@@ -38,19 +38,21 @@ class ValuteDynamicTest extends PHPUnit_Framework_TestCase{
 
     // $this->assertInstanceOf('stdClass', $resultArray[0]);
 
-    $this->assertSame($todayString, $resultArray[0]->date, 'Dates should be equal!');
+    $this->assertSame($todayString, $resultArray[0]['date'], 'Dates should be equal!');
 
-    $this->assertNotNull($todayString, $resultArray[0]->value, 'Valute value cant be NULL');
+    $this->assertNotNull($todayString, $resultArray[0]['value'], 'Valute value cant be NULL');
   }
   /** @test */
   public function it_should_work_with_random_range() {
     $range = ['10/07/2015', '20/07/2015'];
     $resultArray = $this->valute->getCourse($range);
 
-    var_dump(str_replace('/', '.', $range[0]));
     $this->assertCount(11, $resultArray, 'Should be 11 result objects in array');
     // $this->assertInstanceOf('stdClass', $resultArray[0]);
-    $this->assertSame(str_replace('/', '.', $range[0]), $resultArray[0]->date, 'First dates should be equal!');
-    $this->assertSame($range[1], $resultArray[10]->date, 'Last dates should be equal!');
+    $this->assertSame($range[0], $resultArray[0]['date'], 'First dates should be equal!');
+    $this->assertSame($range[1], $resultArray[10]['date'], 'Last dates should be equal!');
+    foreach ($resultArray as $key => $value) {
+       $this->assertNotNull($value['value']);
+    }
   }
 }
