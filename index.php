a1444e3a88e9079c312f1156fbdf71945d83d904
flareair <flareair0@gmail.com>
2015-07-23 14:56:53 +0300

    Changed file permissions, added error logging

--- index.php
+++ index.php
@@ -2,8 +2,18 @@
 
 use valute\Api;
 
-ini_set('display_errors',1);
-error_reporting(E_ALL);
+define('DEBUG', true);
+error_reporting(E_ALL | E_STRICT );
+
+if (DEBUG) {
+  ini_set('display_errors',1);
+  ini_set("log_errors", 0);
+} else {
+  ini_set('display_errors',0);
+  ini_set("log_errors", 1);
+  ini_set("error_log", "./log/php_error.log");
+}
+
 
 $loader = require __DIR__ . '/vendor/autoload.php';
 
