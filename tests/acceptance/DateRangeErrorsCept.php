c4d1180dbbd99f22595bf0d292ec567b6e58102f
flareair <flareair0@gmail.com>
2015-07-24 12:51:43 +0300

    Added acceptance tests for date range input errors

---
+++ tests/acceptance/DateRangeErrorsCept.php
@@ -0,0 +1,43 @@
+<?php
+$I = new AcceptanceTester($scenario);
+$I->wantTo('see right error masseges then input wrong date range or presaved range');
+
+$I->amOnPage('/cb/json/?valute=usd&date1=tomorrow');
+$I->canSeeResponseCodeIs(200);
+$I->see('Wrong date range');
+
+$I->amOnPage('/cb/json/?valute=usd&date1=');
+$I->canSeeResponseCodeIs(200);
+$I->see('Wrong date range');
+
+$I->amOnPage('/cb/json/?valute=usd&date3=undefined');
+$I->canSeeResponseCodeIs(200);
+$I->see('Wrong date range');
+
+$I->amOnPage('/cb/json/?valute=usd&date1=today&date2=01/02/2014');
+$I->canSeeResponseCodeIs(200);
+$I->see('Wrong date range');
+
+$I->amOnPage('/cb/json/?valute=usd&date1=date2=01/02/2014');
+$I->canSeeResponseCodeIs(200);
+$I->see('Wrong date range');
+
+$I->amOnPage('/cb/json/?valute=usd&date2=01/02/2014');
+$I->canSeeResponseCodeIs(200);
+$I->see('Wrong date range');
+
+$I->amOnPage('/cb/json/?valute=usd&date1=01/02/2014');
+$I->canSeeResponseCodeIs(200);
+$I->see('Wrong date range');
+
+$I->amOnPage('/cb/json/?valute=usd&date1=01/02/2014&date2=1.3.2014');
+$I->canSeeResponseCodeIs(200);
+$I->see('Wrong date range');
+
+$I->amOnPage('/cb/json/?valute=usd&date1=01/02/2014&date2=1/3/2014');
+$I->canSeeResponseCodeIs(200);
+$I->see('Wrong date range');
+
+$I->amOnPage('/cb/json/?valute=usd&date1=01/02/2014&date2=1/3/14');
+$I->canSeeResponseCodeIs(200);
+$I->see('Wrong date range');
