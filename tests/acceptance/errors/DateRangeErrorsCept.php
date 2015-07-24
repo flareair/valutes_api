841e6dc93b5faba8eb64193d70a7f34c655c204b
flareair <flareair0@gmail.com>
2015-07-24 15:37:46 +0300

    Now date range test uses url iterator

--- tests/acceptance/errors/DateRangeErrorsCept.php
+++ tests/acceptance/errors/DateRangeErrorsCept.php
@@ -2,50 +2,24 @@
 $I = new AcceptanceTester($scenario);
 $I->wantTo('see right error masseges then input wrong date range or presaved range');
 
-$I->amOnPage('/cb/json/?valute=usd&date1=tomorrow');
-$I->canSeeResponseCodeIs(200);
-$I->see('Wrong date range');
+$urlList = [
+  '/cb/json/?valute=usd&date1=tomorrow',
+  '/cb/json/?valute=usd&date1=',
+  '/cb/json/?valute=usd&date3=undefined',
+  '/cb/json/?valute=usd&date1=today&date2=01/02/2014',
+  '/cb/json/?valute=usd&date1=date2=01/02/2014',
+  '/cb/json/?valute=usd&date2=01/02/2014',
+  '/cb/json/?valute=usd&date1=01/02/2014',
+  '/cb/json/?valute=usd&date1=01/02/2014&date2=1.3.2014',
+  '/cb/json/?valute=usd&date1=01/02/2014&date2=1/3/2014',
+  '/cb/json/?valute=usd&date1=01/02/2014&date2=1/3/14',
+  '/cb/json/?valute=usd&date1=01/02/2015&date2=01/03/2014',
+  '/cb/json/?valute=usd&date1=02/01/2015&date2=01/01/2015',
+];
 
-$I->amOnPage('/cb/json/?valute=usd&date1=');
-$I->canSeeResponseCodeIs(200);
-$I->see('Wrong date range');
 
-$I->amOnPage('/cb/json/?valute=usd&date3=undefined');
-$I->canSeeResponseCodeIs(200);
-$I->see('Wrong date range');
-
-$I->amOnPage('/cb/json/?valute=usd&date1=today&date2=01/02/2014');
-$I->canSeeResponseCodeIs(200);
-$I->see('Wrong date range');
-
-$I->amOnPage('/cb/json/?valute=usd&date1=date2=01/02/2014');
-$I->canSeeResponseCodeIs(200);
-$I->see('Wrong date range');
-
-$I->amOnPage('/cb/json/?valute=usd&date2=01/02/2014');
-$I->canSeeResponseCodeIs(200);
-$I->see('Wrong date range');
-
-$I->amOnPage('/cb/json/?valute=usd&date1=01/02/2014');
-$I->canSeeResponseCodeIs(200);
-$I->see('Wrong date range');
-
-$I->amOnPage('/cb/json/?valute=usd&date1=01/02/2014&date2=1.3.2014');
-$I->canSeeResponseCodeIs(200);
-$I->see('Wrong date range');
-
-$I->amOnPage('/cb/json/?valute=usd&date1=01/02/2014&date2=1/3/2014');
-$I->canSeeResponseCodeIs(200);
-$I->see('Wrong date range');
-
-$I->amOnPage('/cb/json/?valute=usd&date1=01/02/2014&date2=1/3/14');
-$I->canSeeResponseCodeIs(200);
-$I->see('Wrong date range');
-
-$I->amOnPage('/cb/json/?valute=usd&date1=01/02/2015&date2=01/03/2014');
-$I->canSeeResponseCodeIs(200);
-$I->see('Wrong date range');
-
-$I->amOnPage('/cb/json/?valute=usd&date1=02/01/2015&date2=01/01/2015');
-$I->canSeeResponseCodeIs(200);
-$I->see('Wrong date range');
+foreach ($urlList as $key => $url) {
+  $I->amOnPage($url);
+  $I->canSeeResponseCodeIs(200);
+  $I->see('Wrong date range');
+}
