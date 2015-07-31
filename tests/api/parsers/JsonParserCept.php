edc86c5a303d055d5f9c42775ef723e9fd5e7aa5
flareair <flareair0@gmail.com>
2015-07-31 12:21:44 +0300

    Tests for json parser writed

--- tests/api/parsers/JsonParserCept.php
+++ tests/api/parsers/JsonParserCept.php
@@ -2,15 +2,25 @@
 $I = new ApiTester($scenario);
 $I->wantTo('See valid json response then use json data parser');
 
-$I->sendGET('/cb/json/?valute=usd&date1=week');
-$I->canSeeHttpHeader('Content-type', 'application/json');
-$I->seeResponseIsJson();
+$urlList = [
+  '/cb/json/?valute=usd&date1=today',
+  '/cb/json/?valute=usd&date1=week',
+  '/cb/json/?valute=usd&date1=10/05/2015&date2=12/05/2015',
+  '/cb/json/?valute=usd&date1=10/05/2015&date2=10/05/2015',
+];
 
-$I->sendGET('/cb/json/?valute=usd&date1=week');
-$I->canSeeHttpHeader('Content-type', 'application/json');
-$I->seeResponseIsJson();
-$I->seeResponseJsonMatchesJsonPath('//[0]/date');
 
-$I->sendGET('/cb/json/?valute=usd&date1=15/07/2015&date2=20/07/2015');
-$I->canSeeHttpHeader('Content-type', 'application/json');
-$I->seeResponseIsJson();
+foreach ($urlList as $url) {
+  $I->sendGET($url);
+  $I->canSeeHttpHeader('Content-type', 'application/json');
+  $I->seeResponseIsJson();
+  $I->seeResponseJsonMatchesJsonPath('$.[*].date');
+  $I->seeResponseJsonMatchesJsonPath('$.[*].nominal');
+  $I->seeResponseJsonMatchesJsonPath('$.[*].value');
+}
+
+$I->sendGET($urlList[2]);
+$I->seeResponseEquals('[{"date":"10/05/2015","nominal":"1","value":"50,7511"},{"date":"11/05/2015","nominal":"1","value":"50,7511"},{"date":"12/05/2015","nominal":"1","value":"50,7511"}]');
+
+$I->sendGET($urlList[3]);
+$I->seeResponseEquals('[{"date":"10/05/2015","nominal":"1","value":"50,7511"}]');
