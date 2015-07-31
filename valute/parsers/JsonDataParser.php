ab35e3a7bd8ce62cc0e5355c3ada8eb47b088fb5
flareair <flareair0@gmail.com>
2015-07-31 12:21:25 +0300

    Fixed problem with json parser, slashes now not escaped

--- valute/parsers/JsonDataParser.php
+++ valute/parsers/JsonDataParser.php
@@ -6,7 +6,7 @@ use valute\interfaces\DataParser;
 
 class JsonDataParser implements DataParser {
   public function parse(array $data) {
-    return json_encode($data);
+    return json_encode($data, JSON_UNESCAPED_SLASHES);
   }
   public function getHeader() {
     return 'Content-Type: application/json';
